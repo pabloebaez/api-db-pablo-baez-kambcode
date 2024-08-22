d40010fd7145b74d9262fca04da3697c1f8d14a6
Pablo Baez <pablobaez2005@gmail.com>
2024-08-21 19:23:14 -0500

    Organizo index y app.js revision

--- src/app.js
+++ src/app.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const studentRoutes = require('./routes/studentRoutes');
+const courseRoutes = require('./routes/courseRoutes');
+const enrollmentRoutes = require('./routes/enrollmentRoutes');
+
 const app = express();
 
+// Middleware para procesar JSON
 app.use(express.json());
 
+// Configuración de las rutas
+app.use('/students', studentRoutes);
+app.use('/courses', courseRoutes);
+app.use('/enrollments', enrollmentRoutes);
+
+// Exportación del módulo app para que pueda ser usado en index.js
 module.exports = app;
